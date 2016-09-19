28674f26faa7f5bc394c15f33bb9d62a1e67472e
jfed8 <jadenfeddock@gmail.com>
2016-09-19 14:16:50 -0600

    Finished initial setup

--- DataStructureBasic/Program.cs
+++ DataStructureBasic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,56 @@ namespace DataStructureBasic
 {
     class Program
     {
+        public static Random random = new Random();
+
+        public const int CUSTOMERS = 100;
+        public const int PAD = 30;
+
+        public static string randomName()
+        {
+            string[] names = new string[8] { "Dan Morain", "Emily Bell", "Carol Roche", "Ann Rose", "John Miller", "Greg Anderson", "Arthur McKinney", "Joann Fisher" };
+            int randomIndex = Convert.ToInt32(random.NextDouble() * 7);
+            return names[randomIndex];
+        }
+
+        public static int randomNumberInRange()
+        {
+            return Convert.ToInt32(random.NextDouble() * 20);
+        }
+
         static void Main(string[] args)
         {
+            Queue<String> list = new Queue<String>();
+            Dictionary<String, int> dict = new Dictionary<string,int>();
 
+            for (int i=0; i<CUSTOMERS; i++)
+            {
+                list.Enqueue(randomName());
+            }
+
+            IEnumerator iter = list.GetEnumerator();
+
+            foreach (String name in list)
+            {
+
+                if (dict.ContainsKey(name))
+                {
+                    int old;
+                    dict.TryGetValue(name, out old);
+                    dict[name] = old + randomNumberInRange();
+                }
+                else
+                {
+                    dict.Add(name, randomNumberInRange());
+                }
+            }
+
+            foreach (KeyValuePair<String, int> cust in dict)
+            {
+                Console.WriteLine(cust.Key.PadRight(PAD, ' ') + cust.Value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
